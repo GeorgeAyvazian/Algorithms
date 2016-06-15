aa66079dd25d68a819f9487932c3d4168c9e366d
George Ayvazian <george.ayvazian@am.sony.com>
2016-06-14 18:52:10 -0700

    wip for object graph paths

--- src/algorithms/FileWalking.java
+++ src/algorithms/FileWalking.java
@@ -1,8 +1,11 @@
 package algorithms;
 
+import datastructures.graphs.Graph;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,14 +15,19 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -44,35 +52,66 @@ public class FileWalking {
         workspaceLines.add(breakpointStart, "<breakpoints>");
         AtomicInteger atomicInteger = new AtomicInteger(breakpointStart);
         String pattern = Pattern.compile(".*" + args[1].toLowerCase() + ".*?\\{\\s*$").pattern();
-        Consumer<Path> fileOperation = path -> {
+        Pattern memberP = Pattern.compile("^\\s*?(private|protected)\\s*?([A-Z][^<|^>]+?)\\s+.*;.*");
+        Map<String, List<String>> stringArrayListHashMap = new HashMap<>();
+        Consumer<Path> memberOperation = path -> {
             try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(path.toString())))) {
                 List<String> lines = bufferedReader.lines().collect(Collectors.toList());
-                for (String s : lines) {
-                    if (s.toLowerCase().matches(pattern)) {
-                        String line1 = "<line-breakpoint enabled=\"true\" type=\"java-line\">" +
-                                "<url>file://" + path.toString().replace(projectRootPath, "$PROJECT_DIR$") + "</url>" +
-                                "<line>" + lines.indexOf(s) + "</line>" +
-                                "<properties/>" +
-                                "</line-breakpoint>";
-                        if (workspaceLines.indexOf(line1) == -1) {
-                            workspaceLines.add(atomicInteger.incrementAndGet(), line1);
-                        }
-                        String line2 = "<line-breakpoint enabled=\"true\" type=\"java-line\">" +
-                                "<url>file://" + path.toString().replace(projectRootPath, "$PROJECT_DIR$") + "</url>" +
-                                "<line>" + (lines.indexOf(s) + 1) + "</line>" +
-                                "<properties/>" +
-                                "</line-breakpoint>";
-                        if (workspaceLines.indexOf(line2) == -1) {
-                            workspaceLines.add(atomicInteger.incrementAndGet(), line2);
+                for (String line : lines) {
+                    Matcher matcher = memberP.matcher(line);
+                    if (matcher.matches()) {
+                        String node = path.getFileName().toString().replace(".java", "");
+                        List<String> strings1 = stringArrayListHashMap.get(node.toLowerCase());
+                        String group = matcher.group(2);
+                        if (strings1 == null) {
+                            List<String> strings = new ArrayList<>();
+                            strings.add(group.toLowerCase());
+                            stringArrayListHashMap.put(node.toLowerCase(), strings);
+                        } else {
+                            if (!strings1.contains(group)) {
+                                strings1.add(group.toLowerCase());
+                            }
                         }
                     }
                 }
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
         };
-        JavaFileVisitor visitor = new JavaFileVisitor(fileOperation);
+
+        Consumer<Path> fileOperation = path -> {
+            try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(path.toString())))) {
+                List<String> lines = bufferedReader.lines().collect(Collectors.toList());
+                lines.stream().filter(s -> s.toLowerCase().matches(pattern)).forEach(s -> {
+                    String line1 = "<line-breakpoint enabled=\"true\" type=\"java-line\">" +
+                            "<url>file://" + path.toString().replace(projectRootPath, "$PROJECT_DIR$") + "</url>" +
+                            "<line>" + lines.indexOf(s) + "</line>" +
+                            "<properties/>" +
+                            "</line-breakpoint>";
+                    if (workspaceLines.indexOf(line1) == -1) {
+                        workspaceLines.add(atomicInteger.incrementAndGet(), line1);
+                    }
+                    String line2 = "<line-breakpoint enabled=\"true\" type=\"java-line\">" +
+                            "<url>file://" + path.toString().replace(projectRootPath, "$PROJECT_DIR$") + "</url>" +
+                            "<line>" + (lines.indexOf(s) + 1) + "</line>" +
+                            "<properties/>" +
+                            "</line-breakpoint>";
+                    if (workspaceLines.indexOf(line2) == -1) {
+                        workspaceLines.add(atomicInteger.incrementAndGet(), line2);
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        };
+//        JavaFileVisitor visitor = new JavaFileVisitor(fileOperation);
+        JavaFileVisitor visitor = new JavaFileVisitor(memberOperation);
         Files.walkFileTree(projectRoot, visitor);
+        Graph<String> stringGraph = new Graph<>(stringArrayListHashMap);
+        System.out.println("==============>" + stringGraph.breadthFirstSearch("sku", "partner"));
+
+        System.out.println(stringArrayListHashMap);
         workspaceLines.add(atomicInteger.incrementAndGet(), "</breakpoints>");
         workspaceLines.add(atomicInteger.incrementAndGet(), "<breakpoints-dialog>");
         workspaceLines.add(atomicInteger.incrementAndGet(), "<breakpoints-dialog />");
